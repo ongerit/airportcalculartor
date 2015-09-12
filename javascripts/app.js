2ee7cefa971989dcccbf35b749c99e3fcf42d721
Thomas Ongeri <ongerit@gmail.com>
2015-09-12 20:25:29 +0000

    moved text to js file

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,44 +1,108 @@
 $(document).ready(function(){
-var geocoder;
-var map;
-var infowindow = new google.maps.InfoWindow();
-var marker;
-function initialize() {
-  geocoder = new google.maps.Geocoder();
-  var latlng = new google.maps.LatLng(40.730885,-73.997383);
-  var mapOptions = {
-    zoom: 8,
-    center: latlng,
-    mapTypeId: 'roadmap'
-  }
-  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-}
-
-function codeLatLng() {
-  var input = document.getElementById('latlng').value;
-  var latlngStr = input.split(',', 2);
-  var lat = parseFloat(latlngStr[0]);
-  var lng = parseFloat(latlngStr[1]);
-  var latlng = new google.maps.LatLng(lat, lng);
-  geocoder.geocode({'latLng': latlng}, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      if (results[1]) {
-        map.setZoom(11);
-        marker = new google.maps.Marker({
-            position: latlng,
-            map: map
+    var airports = "javascripts/data.json";
+    $.getJSON(airports).done(function(data){
+      
+      
+      var data;
+      var airportName = [];
+      var airportLong = [];
+      var airportLat = [];
+      
+    
+      for( var i in data) {
+      
+      airportName.push(data[i].Airport_Name);
+      airportLong.push(data[i].Longitude);
+      airportLat.push(data[i].Latitute); 
+      
+        };
+        
+        $("#origin,#end").autocomplete({
+          source: airportName
+        
         });
-        infowindow.setContent(results[1].formatted_address);
-        infowindow.open(map, marker);
-      } else {
-        alert('No results found');
-      }
-    } else {
-      alert('Geocoder failed due to: ' + status);
-    }
+    
+    
+      $("#btnSubmit").click(function(){
+       
+       var valOrigin = $("#origin").val();
+       var valEnd = $("#end").val();
+       //alert(valOrigin + valEnd );
+       
+       var indexForOrigin = airportName.indexOf(valOrigin);
+       var indexForDest = airportName.indexOf(valEnd);
+       
+       var lon1, long2, lat1, lat2;
+       
+        long1 = airportLong[indexForOrigin];
+        lat1 = airportLat[indexForOrigin];
+        long2 = airportLong[indexForDest];
+        lat2 = airportLat[indexForDest];
+        
+                  
+              // round to the nearest 1/1000
+        function round(x) {
+        	return Math.round( x * 1000) / 1000;
+        }
+          
+          
+  	      // convert degrees to radians
+        	function deg2rad(deg) {
+        		rad = deg * Math.PI/180; // radians = degrees * pi/180
+        		return rad;
+        	}
+        	
+        	
+    
+        
+        function calculateDist(){
+          
+          
+          // convert coordinates to radians
+        	lat1 = deg2rad(lat1);
+        	long1 = deg2rad(long1);
+        	lat2 = deg2rad(lat2);
+        	long2 = deg2rad(long2);
+
+          var R = 3961; //miles
+          var dlon = long2 - long1;
+          var dlat = lat2 - lat1;
+          
+          //var a = Math.pow((Math.sin(dlat/2)),2) + Math.cos(originLat) * Math.cos(destLat) * Math.pow((Math.sin(dlon/2)),2);
+          var a = Math.pow(Math.sin(dlat/2),2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dlon/2),2);
+          var c = 2 * Math.atan2(Math.sqrt(a),Math.sqrt(1-a)); 
+          var d = R * c; //(where R is the radius of the Earth)
+          var dm = round(d);
+
+          console.log(d);
+          console.log(dm);
+          
+          
+          $('#total').text(dm+ " M");
+
+          //console.log(dlon)
+          //console.log(dlat)
+          //console.log(destLat)
+
+        
+        };
+        
+        
+        calculateDist();
+
+  
+      
+            console.log(lat1 +" "+ long1 +" "+ lat2+" "+ long2);
+
+    
+
   });
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
-
+      
     });
+  
+
+  
+
+  
+  
+});
