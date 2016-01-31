e69c2fab1404487e93f078c514ca89e313ed9d68
Thomas Ongeri <ongerit@gmail.com>
2016-01-30 21:36:03 -0500

    animations are next

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -9,12 +9,20 @@ var vm = new Vue({
         destination: '',
         airports: '',
         airportName: [],
-        distance: []
+        airportNameLatLong: [],
+        distance: [],
+        lat1: '',
+        lat2: '',
+        long1: '',
+        long2: '',
+        totalDistance: '',
+        indexOfOrigin: '',
+        indexOfDestination: '',
+        show: 'false'
     },
 
     ready: function() {
         this.getData();
-        this.getDistance();
 
     },
     methods: {
@@ -24,18 +32,18 @@ var vm = new Vue({
                 this.$set('airports', data);
                 this.getAirports();
                 this.typeData();
+                this.getIndex();
             });
         },
         getAirports: function() {
             var airportData = this.airports;
             for (var i = 0; i < airportData.length; i++) {
-
                 this.airportName.push(airportData[i].an + ' ( ' + airportData[i].ac + ' ) ');
                 //                this.$set('airportName', airportData.a[i]);
 
             }
         },
-
+        
         typeData: function() {
 
             var a = vm._data.airportName;
@@ -60,37 +68,77 @@ var vm = new Vue({
 
 
         },
+        
+        getIndex:  function(){
+                var  org = this.airportName.indexOf(this.origin);
+                var dest = this.airportName.indexOf(this.destination);
+                this.$set('indexOfOrigin', org);
+                this.$set('indexOfDestination', dest);
+        },
+
 
         getDistance: function(origin, destination) {
-            var apiKey, url, origin, destination;
-
-            var origin = 'Seattle';
-            var destination = 'San+Francisco';
-
-            this.$set('origin', origin);
-            this.$set('destination', destination);
-            apiKey = 'AIzaSyDnzIlVM67hNWLuBmw2HPlFL7Apsk8Pbhw';
-            url = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + origin + '&destinations=' + destination + '&outputFormat=application/json&key=' + apiKey;    
             
-            this.$http.jsonp(url).then(function(response) {
+            this.getIndex();
+
+            var lat1,long1,lat2,long2;
             
-                var data = response.data,
-                    status = response.status,
-                    headers = response.headers,
-                    config = response.config;
-                    console.log(data,status,headers,config);
+            console.log(this.indexForOrigin);
+            
+        
+            if(origin > -1 && destination > -1){
                 
-                return data; 
+            lat1 = this.airports[origin]['la'];
+            long1 = this.airports[origin]['lo'];
+            lat2 = this.airports[destination]['la'];
+            long2 = this.airports[destination]['lo']; 
                 
-                }, function(response) {
-
-                }, {
-                    'jsonp': 'callback'
-                }
-
-            );
+            console.log(lat1);
+            console.log(this.airports[ 3 ]['Country']);
             
+                        
+
+            // round to the nearest 1/1000
+            function round(x) {
+                return Math.round(x * 10) / 10;
+            }
+
+            // convert degrees to radians
+            function deg2rad(deg) {
+                rad = deg * Math.PI / 180; // radians = degrees * pi/180
+                return rad;
+            }
             
+                // convert coordinates to radians
+                lat1 = deg2rad(lat1);
+                long1 = deg2rad(long1);
+                lat2 = deg2rad(lat2);
+                long2 = deg2rad(long2);
+
+                var R = 3961; //miles
+                var dlon = long2 - long1;
+                var dlat = lat2 - lat1;
+
+                //var a = Math.pow((Math.sin(dlat/2)),2) + Math.cos(originLat) * Math.cos(destLat) * Math.pow((Math.sin(dlon/2)),2);
+                var a = Math.pow(Math.sin(dlat / 2), 2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dlon / 2), 2);
+                var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+                var d = R * c; //(where R is the radius of the Earth)
+                var dm = round(d);
+
+                console.log(d);
+                console.log(dm);
+                
+                this.$set('totalDistance', dm);
+
+
+                $('#total').text(dm + " M");
+
+            }else{
+                console.log('Please Select an Airport');
+            }
+
+            
+
 
         }
 
